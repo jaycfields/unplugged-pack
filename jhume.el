92106e95750bcfef6fdea93479f841bad6d0d1ed
John Hume <duelin.markers@gmail.com>
2013-08-20 10:20:06 -0500

    Assorted junk to make graphical emacs on Ubuntu work

--- jhume.el
+++ jhume.el
@@ -1,1 +1,15 @@
-(live-set-default-font "Source Code Pro 12")
+(if (string= system-type "darwin")
+    (live-set-default-font "Source Code Pro 12"))
+
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (replace-regexp-in-string
+                          "[ \t\n]*$"
+                          ""
+                          (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq eshell-path-env path-from-shell) ; for eshell users
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(if window-system (set-exec-path-from-shell-PATH))
+
+(setenv "LEIN_JVM_OPTS" "")
